3a3f8762a788c3a18057e6e78e8858838e6c08f5
Emilly <millyphb@gmail.com>
2023-05-01 17:23:36 -0300

    atualizando textformfield, adicionando typefaces, e scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
+
+var maskFormatterCPF = new MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
+
+var maskFormatterCNPJ = new MaskTextInputFormatter(
+    mask: "##.###.###/####-##",
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
 
 class MyApp extends StatelessWidget {
   @override
@@ -22,9 +34,33 @@ class WidgetsBasicos extends StatelessWidget {
       appBar: AppBar(
         title: Text("Cadastre-se"),
       ),
-      body: Container(
-        color: Colors.white,
-        child: widgetTextField(),
+      body: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+              ),
+              Text(
+                'Criar Conta',
+                textDirection: TextDirection.ltr,
+                style: GoogleFonts.getFont('Montserrat', fontSize: 30.0),
+              ),
+              widgetTextField(),
+              Text(
+                'Qual modalidade?',
+                textDirection: TextDirection.ltr,
+                style: GoogleFonts.getFont('Montserrat', fontSize: 20.0),
+              ),
+              textFieldModalidade(),
+              Text(
+                'Qual categoria?',
+                textDirection: TextDirection.ltr,
+                style: GoogleFonts.getFont('Montserrat', fontSize: 20.0),
+              ),
+              textFieldCategoria(),
+            ]),
       ),
     );
   }
@@ -34,70 +70,178 @@ class WidgetsBasicos extends StatelessWidget {
       padding: const EdgeInsets.all(35.0),
       child: Column(
         children: [
-          TextField(
-            autofocus: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.greenAccent, width: 3.0),
-                borderRadius: new BorderRadius.circular(25.0),
-              ),
-              hintText: 'Nome',
-              labelStyle: TextStyle(
-                fontSize: 50,
-                color: Colors.green,
-              ),
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              fillColor: Colors.greenAccent,
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.greenAccent, width: 3.0),
-                borderRadius: new BorderRadius.circular(25.0),
-              ),
-              hintText: 'Email',
-              labelStyle: TextStyle(
-                fontSize: 50,
-                color: Colors.green,
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+            height: 50,
+            width: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.green,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Nome',
+                labelStyle: TextStyle(
+                  fontSize: 50,
+                  color: Colors.green,
+                ),
               ),
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.greenAccent, width: 3.0),
-                borderRadius: new BorderRadius.circular(25.0),
-              ),
-              hintText: 'Senha',
-              labelStyle: TextStyle(
-                fontSize: 50,
-                color: Colors.green,
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+            height: 50,
+            width: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.green,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                labelStyle: TextStyle(
+                  fontSize: 50,
+                  color: Colors.green,
+                ),
               ),
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.greenAccent, width: 3.0),
-                borderRadius: new BorderRadius.circular(25.0),
-              ),
-              hintText: 'Confirme a Senha',
-              labelStyle: TextStyle(
-                fontSize: 50,
-                color: Colors.green,
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+            height: 50,
+            width: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.green,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Senha',
+                labelStyle: TextStyle(
+                  fontSize: 50,
+                  color: Colors.green,
+                ),
               ),
             ),
           ),
-          ElevatedButton(
-            style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30.0),
+            height: 50,
+            width: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.green,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Repita a Senha ',
+                labelStyle: TextStyle(
+                  fontSize: 50,
+                  color: Colors.green,
+                ),
+              ),
             ),
-            child: const Text('Avançar'),
-            onPressed: () {
-              // ...
-            },
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  Padding textFieldModalidade() {
+    return Padding(
+      padding: const EdgeInsets.all(35.0),
+      child: Container(
+        margin: EdgeInsets.only(top: 30.0),
+        height: 50,
+        width: 500,
+        decoration: BoxDecoration(
+            border: Border.all(
+              width: 2.0,
+              color: Colors.green,
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0),
+              bottomLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0),
+            )),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Confirme a Senha',
+            labelStyle: TextStyle(
+              fontSize: 50,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding textFieldCategoria() {
+    return Padding(
+      padding: const EdgeInsets.all(35.0),
+      child: Container(
+        margin: EdgeInsets.only(top: 30.0),
+        height: 50,
+        width: 500,
+        decoration: BoxDecoration(
+            border: Border.all(
+              width: 2.0,
+              color: Colors.green,
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0),
+              bottomLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0),
+            )),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Confirme a Senha',
+            labelStyle: TextStyle(
+              fontSize: 50,
+              color: Colors.green,
+            ),
+          ),
+        ),
       ),
     );
   }
